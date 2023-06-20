b625f84ca8e4c5585dab6000cc35f0290e6f0d67
bitvoyager <tigerrabbit22@outlook.com>
2023-06-20 05:42:59 -0700

    ss

--- readData.py
+++ readData.py
@@ -12,77 +12,84 @@ import matplotlib.pyplot as plt
 from pillow_heif import register_heif_opener
 
 
+strategy = tf.distribute.MirroredStrategy()
+with strategy.scope():
 
-class OnlineKMeans:
-    def __init__(self, threshold):
-        self.threshold = threshold
-        self.clusters = []
-    
-    def predict(self, x):
-        if len(self.clusters) == 0:
-            return -1
+    class OnlineKMeans:
+        def __init__(self, threshold):
+            self.threshold = threshold
+            self.clusters = []
         
-        distances = cdist([x], self.clusters, metric='euclidean')
-        min_distance = np.min(distances)
+        def predict(self, x):
+            if len(self.clusters) == 0:
+                return -1
+            
+            distances = cdist([x], self.clusters, metric='euclidean')
+            min_distance = np.min(distances)
+            
+            if min_distance > self.threshold:
+                return -1
+            
+            return np.argmin(distances)
         
-        if min_distance > self.threshold:
-            return -1
+        def update(self, x):
+            cluster = self.predict(x)
+            
+            if cluster == -1:
+                self.clusters.append(x)
+                return len(self.clusters) - 1
+            else:
+                self.clusters[cluster] = self.clusters[cluster] * 0.7 + x * 0.3
+                return cluster
+    online_kmeans = OnlineKMeans(threshold=0.5)
+    def classify_image(url):
+        feature_vector = create_feature_vector(url)
+        label = online_kmeans.update(feature_vector)
+        return label
+
+    register_heif_opener()
+
+    def load_and_preprocess_image(url):
+        response = requests.get(url)
         
-        return np.argmin(distances)
-    
-    def update(self, x):
-        cluster = self.predict(x)
         
-        if cluster == -1:
-            self.clusters.append(x)
-            return len(self.clusters) - 1
-        else:
-            self.clusters[cluster] = self.clusters[cluster] * 0.7 + x * 0.3
-            return cluster
-online_kmeans = OnlineKMeans(threshold=0.5)
-def classify_image(url):
-    feature_vector = create_feature_vector(url)
-    label = online_kmeans.update(feature_vector)
-    return label
+        img = Image.open(BytesIO(response.content))
 
-register_heif_opener()
+        img.thumbnail((512, 512))
+        img = img.resize((224, 224))
+        img = np.array(img) / 255.0
+        img = np.expand_dims(img, axis=0)
+        return img  
+    base_model = tf.keras.applications.VGG19(weights='imagenet', include_top=False)
 
-def load_and_preprocess_image(url):
-    response = requests.get(url)
-    
-    
-    img = Image.open(BytesIO(response.content))
 
-    img.thumbnail((512, 512))
-    img = img.resize((224, 224))
-    img = np.array(img) / 255.0
-    img = np.expand_dims(img, axis=0)
-    return img  
-base_model = tf.keras.applications.VGG19(weights='imagenet', include_top=False)
-
-def create_feature_vector(url):
-    img = load_and_preprocess_image(url)
-    feature_vector = base_model.predict(img)
-    feature_vector = np.reshape(feature_vector, (7*7*512))
-    return feature_vector
+    def create_feature_vector(url):
+        img = load_and_preprocess_image(url)
+        feature_vector = base_model.predict(img)
+        feature_vector = np.reshape(feature_vector, (7*7*512))
+        return feature_vector
 
 
 
 
-df = pd.read_csv('Sport_Shoes.csv')
-data = df[["category_id", "sub_category_id", "sub_sub_category_id", "brand_id", "image", "chum_product_id"]]
-groupedData = data.groupby(["brand_id", "category_id", "sub_category_id", "sub_sub_category_id"]).agg({"image": lambda x: list(x), "chum_product_id" : lambda x: list(x)})
-
-
-for everyCategory in groupedData.to_numpy():
-    for everyImageUrlArray in everyCategory[0]:
-        array = ast.literal_eval(everyImageUrlArray)
-        print(array[0])
-        print(classify_image(array[0]))
-        # for everyImageUrl in array:
-        #     print(classify_image(everyImageUrl))
-    
-        
-    break
+    df = pd.read_csv('Sport_Shoes_1.csv')
+    data = df[["category_id", "sub_category_id", "sub_sub_category_id", "brand_id", "image", "chum_product_id"]]
+    groupedData = data.groupby(["brand_id", "category_id", "sub_category_id", "sub_sub_category_id"]).agg({"image": lambda x: list(x), "chum_product_id" : lambda x: list(x)})
+    import csv
+    cnt = 0
+    filename = "classification_records.csv"
+    fields = ['Image_url','chum_product_id', 'classID'] 
+    csvFile = open(filename, 'w')
+    csvwriter = csv.writer(csvFile)
+    csvwriter.writerow(fields)
+    for everyCategory in groupedData.to_numpy():
+        for everyImageUrlArray in everyCategory[0]:
+            array = ast.literal_eval(everyImageUrlArray)
+            print(array[0], everyCategory[1][cnt])
+            
+            print(classify_image(array[0]))
+            csvwriter.writerows([[array[0], everyCategory[1][cnt], classify_image(array[0])]])
+            cnt += 1
+        break
 
 
